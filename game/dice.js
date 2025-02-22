170022d372bf5cb4b557e10e90132b9f2f2af81f
KimJack0429 <minsubkim7349@gmail.com>
2025-02-22 02:50:24 +0000

    new file:   game/dice.css 	new file:   game/dice.html 	new file:   game/dice.js 	modified:   index.html

---
+++ game/dice.js
@@ -0,0 +1,18 @@
+
+funtion rollDice() {
+    let maxValue = parselnt(document.getElementById('maxvalue').value);
+    let diceCount = parselnt(document.getElementById('diceCount').value);
+    let modifier = parselnt(document.getElementById('modifier').value);
+    let total = 0
+
+    for (let i = 0, i < diceCount; i++) {
+    let roll = Math.floor(Math.random() * maxValue) + 1;
+    total += roll;
+    resultText += `[${roll}]`;
+    }
+
+    total += modifier;
+    resultText += `+ ${modifier} = ${total}`;
+
+    document.getElementById('result').innerText = resultText
+}

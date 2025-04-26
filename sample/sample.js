3eccb6909c49bb1a56efc9134f0280150d7de7c7
KimJack0429 <minsubkim7349@gmail.com>
2025-04-26 01:49:04 +0000

    modified:   sample/sample.js

---
+++ sample/sample.js
@@ -0,0 +1,22 @@
+const chatData = {
+    '엄마' : [
+        {type: 'received', text: '밥 먹었니?'},
+        {type: 'sent', text: '응 밤금 먹었어!'},
+        {type: 'received', text: '그래~ 오늘도 힘내 ^^'}
+    ],
+    '친구들' : [
+        {type: 'sent', text: '오늘 저녁에 뭐할래?'},
+        {type: 'received', text: '영화 보러 갈래?'},
+        {type: 'sent', text: '좋아, 몇시?'}
+    ],
+    '스터디방' : [
+        {type: 'received', text: '다음 주 과제 정리했어?'},
+        {type: 'sent', text: '응, 구글 드라이브에 올렸어.'},
+        {type: 'received', text: '굿굿! 확인해볼게'}
+    ],
+    '개발자 모임' : [
+        {type: 'sent', text: 'React 버전 올려도 될까요?'},
+        {type: 'received', text: '네. 올려도 괜찮아요!'},
+        {type: 'sent', text: '그럼 오늘 저녁에 반영할게요'}
+    ]
+};
